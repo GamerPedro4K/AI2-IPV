44d06f6da449bbc71a2bd2ba09f9ff3eb58393a6
GamerPedro4K <pedro.t.carvalho12345@gmail.com>
2024-05-03 01:27:48 +0100

    a

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -11,6 +11,7 @@ import CardC from '../components/Card';
 const Home = (props) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [msgLoad, setMsgLoad] = useState('Carregando dados...');
 
     const fetchData = async () => {
         try {
@@ -23,8 +24,8 @@ const Home = (props) => {
             }
             
         } catch (error) {
+            setMsgLoad('Erro ao carregar dados');
             console.error('Error fetching data:', error);
-            setLoading(false);
         }
     };
     
@@ -34,14 +35,19 @@ const Home = (props) => {
     }, []); 
 
 
-
-
     return (
         <>
        <NavbarC/>
 
         {loading ? (
-        <p>Loading...</p>
+            <div className='centered'>
+                 <div className='d-flex justify-content-center flex-column align-items-center'>
+                    {msgLoad === 'Carregando dados...' ? <img src="https://i.pinimg.com/originals/f9/41/ae/f941ae9d16fd7d2957eea6e5b1100d1e.gif" style={{maxWidth: "1000px"}} alt=""/> : <img src="https://i.gifer.com/origin/78/787899e9d4e4491f797aba5c61294dfc_w200.gif" style={{maxWidth: "1000px"}} alt=""/>}
+                
+                <h1>{msgLoad}</h1>
+            </div>
+            </div>
+           
       ) : (
         <div>
             {<Slider items={[data[0] || null, data[1] || null, data[2] || null, data[3] || null, data[4] || null]}/>}
