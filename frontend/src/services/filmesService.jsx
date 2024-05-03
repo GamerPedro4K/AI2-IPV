482f60954829803fb6f48ce9101df04858711734
GamerPedro4K <pedro.t.carvalho12345@gmail.com>
2024-05-03 04:35:57 +0100

    a

--- frontend/src/services/filmesService.jsx
+++ frontend/src/services/filmesService.jsx
@@ -1,18 +1,19 @@
 // filmesService.js
-
 import config from '../data/config';
 
-config.apiUrl = config.apiUrl + '/filme/';
 
 export const getFilmes = async () => {
     try {
-        const response = await fetch(config.apiUrl + 'list');
+        const response = await fetch(config.apiUrl + '/filme/list');
         const data = await response.json();
         if(data.status === 200){
             return data.success.msg;
         }
+        else{
+            throw new Error(data.errors.msg_error);
+        }
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.log('Error fetching data:', error);
         throw error;
     }
 };
